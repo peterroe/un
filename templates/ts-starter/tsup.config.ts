94438556349e5b972cd612eb74ba93e0bb5a97bc
linshuheng03 <linshuheng03@kuaishou.com>
2023-09-23 14:00:15 +0800

    fix: lack of `typescript` in ts-starter

---
+++ templates/ts-starter/tsup.config.ts
@@ -0,0 +1,13 @@
+import { builtinModules } from 'module'
+import { defineConfig } from 'tsup'
+
+import pkg from './package.json'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  clean: true,
+  minify: true,
+  format: ['esm', 'cjs'],
+  external: [...builtinModules, ...Object.keys(pkg.dependencies || {})],
+  dts: true,
+})
